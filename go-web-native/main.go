bfd05f84ac158bac5e96fe41f93a960c035975b4
Rizaldi Yusuf <codingzaldi@gmail.com>
2023-12-25 03:20:19 +0700

    first

---
+++ go-web-native/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"go-web-native/config"
+	"go-web-native/controllers/categorycontroller"
+	"go-web-native/controllers/homecontroller"
+	"go-web-native/controllers/productcontroller"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	//homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	//categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	//products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+
+}
